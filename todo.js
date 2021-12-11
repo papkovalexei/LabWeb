7777347e9e1359c1d0a11b45143cd3c336bb2b60
Alexei Papkov <steam07151@gmail.com>
2021-12-11 19:13:07 +0300

    Lab6-7

--- todo.js
+++ todo.js
@@ -1,32 +1,7 @@
 window.onload = function() {
-    var body = document.getElementById("containertable");
-    var table = document.createElement("table");
-    var thead = document.createElement("thead");
-    var tr = document.createElement("tr");
-
-    let headName = ["Название", "Описание", "Исполнитель"];
-
-    for (var i = 0; i < headName.length; i++) {
-        console.log(i);
-        var thHelp = document.createElement("th");
-        thHelp.setAttribute("scope", "col");
-        var text = document.createTextNode(headName[i]);
-        thHelp.appendChild(text);
-        tr.appendChild(thHelp);
-
-        var tdHelp = document.createElement("td");
-        tdHelp.setAttribute("data-label", headName[i]);
-    }
-
-    thead.appendChild(tr);
-    var tbody = document.createElement("tbody");
-    tbody.setAttribute("id", "tabletodo");
-
-    table.appendChild(thead);
-    table.appendChild(tbody);
-    body.appendChild(table);
-    var tableTemplate = document.querySelector("#tabletemplate");
-
+    var table = document.getElementById("tabletodo");
+    var tableTemplate = document.getElementById("tabletemplate");
+    console.log(localStorage);
     for (var i = 0; i < localStorage.length; i++) {
         var data = JSON.parse(localStorage[i]);
         var newC = tableTemplate.content.firstElementChild.cloneNode(true);;
@@ -34,7 +9,69 @@ window.onload = function() {
         rd[0].textContent = data["nameTask"];
         rd[1].textContent = data["desc"];
         rd[2].textContent = data["who"];
-        table.getElementsByTagName("tbody")[0].appendChild(newC);
+        table.appendChild(newC);
+    }
+
+    let frm = 
+    `<form>
+    <div class="mb-3">
+        <label for="Сделать шото" class="form-label">Название</label>
+        <input type="text" data-name="nameTask" class="form-control" id="nametask" required>
+        <label for="описать шото)" class="form-label">Описание</label>
+        <input type="text" data-name="desc" class="form-control" id="desctask" required>
+        <label for="ктооо" class="form-label">Исполнитель задачи</label>
+        <input type="text" data-name="who" class="form-control" id="whotask" required>
+    </div>
+    </form>`
+
+    document.getElementById('new_task').onclick = async function () {
+        let dlg = new BsDialogs({backdrop: true})
+        dlg.form('Добваление задачи', 'Добавить', frm)
+        let ret = await dlg.onsubmit()
+        submitFunc(ret)
+    }
+
+    document.getElementById('yes_no').onclick = async function () {
+        let ret = await new BsDialogs().yes_no('Очистить задачи', 'Вы в этом уверены????')
+
+        if(ret == "yes")
+            clean();
+    }
+
+    loadComment();
+}
+
+function addComment(name, text) {
+    var placeholder = document.getElementById("commenthere");
+    var commentTemplate = document.getElementById("commenttemplate");
+    var newComment = commentTemplate.content.firstElementChild.cloneNode(true);
+    newComment.getElementsByTagName("div")[0].firstElementChild.textContent += name;
+    newComment.getElementsByTagName("div")[1].firstElementChild.textContent = text;
+    placeholder.appendChild(newComment);
+}
+
+async function loadComment(n) {
+    document.querySelector(".preloader").classList.remove("none");
+    document.querySelector(".neterr").classList.remove("error");
+    console.log(document.querySelector(".neterr"));
+    var url = "https://jsonplaceholder.typicode.com/comments";
+    try {
+        let res = await fetch(url);
+        if (res.ok) {
+            var json = await res.json();
+            console.log(json.filter((x)=>x.id >= Math.random()*1000));
+            var filtered = json.filter((x)=>x.id >= Math.random()*1000);
+            for (var i = 0; i < Math.min(filtered.length, 4); i++) { 
+                addComment(filtered[i].name, filtered[i].body);
+            }
+        } else {
+            document.querySelector(".neterr").classList.add("error");
+            document.querySelector(".neterr").classList.remove("none");
+        }
+        document.querySelector(".preloader").classList.add("none");
+    } catch(e) {
+        document.querySelector(".neterr").classList.remove("none");
+        document.querySelector(".preloader").classList.add("none");
     }
 }
 
@@ -46,21 +83,7 @@ function clean() {
     localStorage.clear();
 }
 
-function cleanField() {
-    var elements = document.getElementById("todoform").elements;
-    elements[0].value = "";
-    elements[1].value = "";
-    elements[2].value = "";
-}
-
-function submitFunc() {
-    var elements = document.getElementById("todoform").elements;
-    var obj ={};
-    for(var i = 0 ; i < elements.length - 1; i++){
-        var item = elements.item(i);
-        obj[item.name] = item.value;
-    }
-
+function submitFunc(obj) {
     var table = document.getElementById("tabletodo");
 
     table = document.getElementById("tabletodo");
